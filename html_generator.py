f86eb09b21991e3a9adb73a7b0743cc23c8f7d94
Andrii Perun <andrewfreelan@gmail.com>
2019-10-10 22:46:48 +0300

    fix: gen function to generate left navbar in html
    xml parsing was deleted
    update todo list

--- html_generator.py
+++ html_generator.py
@@ -1,39 +1,54 @@
 import os
-import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
 
-
-# Walk through path tree
-# Add directories/php files to XML file
-def walk(path):
-    list_of_files = {}
-    data = ET.Element('tree')
-    for (dirpath, dirnames, filenames) in os.walk(path):
-
-        # Append slash if not exists
-        if not dirpath.endswith('\\'):
-            dirpath += '\\'
-
-        folder = ET.SubElement(data, 'folder')
-        folder.set('name', os.path.split(os.path.split(dirpath)[0])[1])
-        for dirname in dirnames:
-            dir = ET.SubElement(folder, 'dir')
-            dir.text = dirname
-        for filename in filenames:
-            if filename.endswith('.php'):
-                file = ET.SubElement(folder, 'file')
-                file.text = filename
-                list_of_files[filename] = os.sep.join([dirpath, filename])
-
-    # create a new XML file with the results
-    mydata = ET.tostring(data).decode()
-    myfile = open("tmp.xml", "w")
-    myfile.write(mydata)
-    myfile.close()
-
-    return list_of_files
-
+# Find depth of path
+def path_depth(path):
+    return path.count('\\') + 1
 
 # Generate html docs
-def __gen__():
-    print(3)
+def __gen__(path):
+    tree = os.walk(path)
+    html_code = '<!DOCTYPE html><html>'
+    html_code += """ <head>
+    <meta charset="UTF-8">
+    <link href="css/treeViewSheet.css" rel="stylesheet" media="all"/>
+    <link href="css/navbar-fixed-left.css" rel="stylesheet" media="all"/>
+    <title>Title</title>
+    </head>"""
+    html_code += '<body><div class="sidenav">'
+    html_code += '<ul id="myUL">'
+    cur_path = path
+    for (dirpath, dirnames, filenames) in tree:
+        dirpath = dirpath.replace('\\\\', '\\')
+        print(dirpath)
+        depth_diff = path_depth(cur_path) - path_depth(dirpath)
+        if depth_diff == 0 and cur_path != path:
+            html_code += '</li>'
+        else:
+            while depth_diff > 0:
+                html_code +='</li>'
+                html_code += '</ul>'
+                depth_diff -= 1
+        dir_name = os.path.split(dirpath)[1]
+        print('dirpath:' + dirpath + ' dir_name: ' + dir_name)
+        html_code += '<li><span class="caret">' + dir_name + '</span>'
+        if dirnames or filenames:
+            html_code += '<ul class="nested">'
+        for filename in filenames:
+            html_code += '<li><a href="#">' + filename + '</a></li>'
+        cur_path = dirpath
+    depth_diff = path_depth(cur_path) - path_depth(path)
+    while depth_diff > 0:
+        html_code += '</li>'
+        html_code += '</ul>'
+        depth_diff -= 1
+    html_code += '</li></ul>'
+    html_code += '</div><script type="text/javascript" src="js/treeView.js"></script></body></html>'
+
+    soup = BeautifulSoup(html_code, 'html.parser')  # make BeautifulSoup
+    pretty_html = soup.prettify()
+
+    h_file = open("web/test.html", "w")
+    h_file.write(pretty_html)
+    h_file.close()
 
